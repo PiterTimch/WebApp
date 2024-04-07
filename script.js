1e2bf1b5ffe437a85c76925ecc966def55b4daf1
PiterTimch <149708697+PiterTimch@users.noreply.github.com>
2024-04-07 20:35:33 +0300

    Додав стиль та наімацію

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 let clickCount = 0;
 const clickButton = document.getElementById('clickButton');
 const clickCountDisplay = document.getElementById('clickCount');
+const clickImage = document.querySelector('button img');
 
 clickButton.addEventListener('click', () => {
     clickCount++;
     clickCountDisplay.textContent = clickCount;
+
+    clickImage.classList.add('animated');
+    setTimeout(() => {
+        clickImage.classList.remove('animated');
+    }, 100);
 });
