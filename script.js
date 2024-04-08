473268c271b4c24cd56dc315d4b2bf955a4785e8
PiterTimch <149708697+PiterTimch@users.noreply.github.com>
2024-04-08 13:59:15 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
-let clickCount = 0;
+let clickCount = 0.0;
+let boost = 0.00001;
+let boosPrice = 0.02;
 const clickButton = document.getElementById('clickButton');
 const clickCountDisplay = document.getElementById('clickCount');
 const clickImage = document.querySelector('button img');
 
+const upgradeButton = document.getElementById('boostButton');
+
 clickButton.addEventListener('click', () => {
-    clickCount++;
-    clickCountDisplay.textContent = clickCount;
+    clickCount += boost;
+    clickCountDisplay.textContent = clickCount.toFixed(5);
 
     clickImage.classList.add('animated');
     setTimeout(() => {
         clickImage.classList.remove('animated');
     }, 100);
+});
+
+upgradeButton.addEventListener('click', () =>{
+    if(clickCount > boosPrice){
+        clickCount -= boosPrice;
+        clickCountDisplay.textContent = clickCount;
+        boost += 0.00001;
+        boosPrice *= 2;
+        clickCountDisplay.textContent = clickCount.toFixed(5);
+    }
 });
