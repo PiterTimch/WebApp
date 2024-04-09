ea04d090ed244285d131b8d033e18055c29e94b7
PiterTimch <149708697+PiterTimch@users.noreply.github.com>
2024-04-09 14:17:46 +0300

    Add files via upload

--- TeleBot/main.py
+++ TeleBot/main.py
@@ -1,6 +1,20 @@
 import telebot
 from telebot import types
 
+import sqlite3
+
+# Підключення до бази даних SQLite
+conn = sqlite3.connect('bot_database.db', check_same_thread=False)
+cursor = conn.cursor()
+
+# Створення таблиці користувачів (якщо не існує)
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY
+    )
+''')
+conn.commit()
+
 # Створення бота
 bot = telebot.TeleBot('7084613005:AAEXhjc_oMzP50OfxuORkeBe2Q2zvFO8YnU')
 
@@ -12,14 +26,21 @@ def create_menu():
     markup.add(about_us_button, play_button)
     return markup
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
     text = "Привіт! Я телеграм-бот."
+
+    user_id = message.chat.id
+    # Перевірка, чи користувач вже існує в базі даних
+    cursor.execute("SELECT id FROM users WHERE id=?", (user_id,))
+    existing_user = cursor.fetchone()
+    if not existing_user:
+        cursor.execute("INSERT INTO users (id) VALUES (?)", (user_id,))
+        conn.commit()
 
     # Відправляємо фото з приєднаним текстом
     photo = open('Images/StartLogo.jpg', 'rb')
     bot.send_photo(message.chat.id, photo, caption=text, reply_markup=create_menu())
-
     photo.close()
 
 # Обробник натискання на кнопки
